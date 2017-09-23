442e6cdba3ead1cfe109c458a43fb72ad64f1917
James Sullivan <jsullivan@cambridgecloudpartners.com>
2017-09-22 21:38:26 -0400

    remove leading whitespace which is rendered in markdown as pre

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
   </head>
   <body>
-    <div id="body">
-      <?php include("README.md"); ?>
-    </div>
+    <div id="body"><?php include("README.md"); ?></div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.7.4/showdown.min.js"></script>
     <script>
       var converter = new showdown.Converter();
