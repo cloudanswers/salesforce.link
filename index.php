ed6c5ccc437b340d58279919fbd6d5580393ed6b
James Sullivan <jsullivan@cambridgecloudpartners.com>
2017-09-22 21:35:49 -0400

    add bootstrap to home page

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <html>
-<body>
-<div id="body">
-<?php include("README.md"); ?>
-</div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.7.4/showdown.min.js"></script>
-<script>
-var converter = new showdown.Converter();
-var e = document.getElementById('body');
-e.innerHTML = converter.makeHtml(e.innerHTML);
-</script>
-</body>
+  <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
+  </head>
+  <body>
+    <div id="body">
+      <?php include("README.md"); ?>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/showdown/1.7.4/showdown.min.js"></script>
+    <script>
+      var converter = new showdown.Converter();
+      var e = document.getElementById('body');
+      e.innerHTML = converter.makeHtml(e.innerHTML);
+    </script>
+  </body>
 </html>
