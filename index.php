c3e543691809418a589ccfb0cbef0e53ba125753
James Sullivan <jsullivan@cambridgecloudpartners.com>
2023-07-04 10:43:24 -0400

    add cloudflare confirmation meta tag

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
   <head>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.css" />
+    <meta name="cf-2fa-verify" content="mJRAlybecw78usDxPLpJ" />
   </head>
   <body>
     <div id="body" class="container"><?php include("README.md"); ?></div>
